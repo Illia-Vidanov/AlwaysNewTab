e1940456f627e313f68656011464409e884ce523
Illia Vidanov <illia.vidanov@gmail.com>
2024-10-30 16:11:50 +0100

    tabs to spaces + populate groups
    Changed all tabs to 2 spaces and added functionality to populate tab groups with new tab

--- popup/popup.js
+++ popup/popup.js
@@ -1,43 +1,56 @@
 document.addEventListener('DOMContentLoaded',
 function()
 {
-	var enabled = document.getElementById("enabled");
-	var move = document.getElementById("move");
-	var close = document.getElementById("close");
-	
-	enabled.addEventListener("change",
-	function()
-	{
-		chrome.storage.sync.set({ enabled: this.checked });
-		chrome.runtime.sendMessage({ msg: "Check" });
-	});
-	move.addEventListener("change",
-	function()
-	{
-		chrome.storage.sync.set({ move: this.checked });
-		chrome.runtime.sendMessage({ msg: "Check" });
-	});
-	close.addEventListener("change",
-	function()
-	{
-		chrome.storage.sync.set({ close: this.checked });
-		chrome.runtime.sendMessage({ msg: "Check" });
-	});
-	
-	chrome.storage.sync.get("enabled",
-	function(data)
-	{
-		enabled.checked = data.enabled;
-	});
-	
-	chrome.storage.sync.get("move",
-	function(data)
-	{
-		move.checked = data.move;
-	});
-	chrome.storage.sync.get("close",
-	function(data)
-	{
-		close.checked = data.close;
-	});
+  var enabled = document.getElementById("enabled");
+  var move = document.getElementById("move");
+  var close = document.getElementById("close");
+  var group = document.getElementById("group");
+  
+  enabled.addEventListener("change",
+  function()
+  {
+    chrome.storage.sync.set({ enabled: this.checked });
+    chrome.runtime.sendMessage({ msg: "Check" });
+  });
+  
+  move.addEventListener("change",
+  function()
+  {
+    chrome.storage.sync.set({ move: this.checked });
+    chrome.runtime.sendMessage({ msg: "Check" });
+  });
+  
+  close.addEventListener("change",
+  function()
+  {
+    chrome.storage.sync.set({ close: this.checked });
+    chrome.runtime.sendMessage({ msg: "Check" });
+  });
+  
+  group.addEventListener("change",
+  function()
+  {
+    chrome.storage.sync.set({ group: this.checked });
+    chrome.runtime.sendMessage({ msg: "Check" });
+  });
+  
+  
+  chrome.storage.sync.get("enabled", function(data){
+    enabled.checked = data.enabled;
+  });
+  
+  chrome.storage.sync.get("move", function(data){
+    move.checked = data.move;
+  });
+  
+  chrome.storage.sync.get("close", function(data){
+    close.checked = data.close;
+  });
+  
+  chrome.storage.sync.get("group", function(data){
+    group.checked = data.group;
+  });
+  
+  
+  chrome.runtime.sendMessage({ msg: "Check" });
 });
--- scripts/background.js
+++ scripts/background.js
@@ -2,15 +2,15 @@
 chrome.tabs.onRemoved.addListener(OnRemoved);
 function OnRemoved(tabId, removeInfo)
 {
-	Check();
+  Check();
 }
 
 // Check if url changed
 chrome.tabs.onUpdated.addListener(OnUpdated);
 function OnUpdated(tabId, changeInfo, tab)
 {
-	if(changeInfo.url)
-		Check();
+  if(changeInfo.url)
+    Check();
 }
 
 // Popup refresh message
@@ -22,37 +22,90 @@ function(request, sender, sendResponse)
 
 function Check()
 {
-	chrome.storage.sync.get("enabled", function(data){
-	if(!data.enabled)
-		return;
-	
-	chrome.tabs.query({windowType:'normal'}, function(tabs){
-	var found = false;
-	for(const tab of tabs)
-	{
-		if(tab.url == "chrome://newtab/" && !tab.pinned)
-		{
-			// Move new tabs forward
-			chrome.storage.sync.get("move", function(data){
-				if(data.move)
-					chrome.tabs.move(tab.id, { index: -1 });
-			});
-			
-			// Close if it's not first new tab
-			if(found)
-			{
-				chrome.storage.sync.get("close", function(data){
-					if(data.close)
-						chrome.tabs.remove(tab.id);	
-				});
-			}
-	
-			found = true;
-		}
-	}
-	
-	if(!found)
-		chrome.tabs.create({ url: "chrome://newtab/", active: false }, function(newTab){});
-	});
-	});
+  chrome.storage.sync.get("enabled", function(data){
+    if(!data.enabled)
+      return;
+    
+    // Normal check
+    chrome.tabs.query({windowType:'normal', groupId:chrome.tabGroups.TAB_GROUP_ID_NONE}, function(tabs){
+      var found = false;
+      for(const tab of tabs)
+      {
+        if(tab.url == "chrome://newtab/" && !tab.pinned)
+        {
+          // Move new tabs forward
+          chrome.storage.sync.get("move", function(data){
+            if(data.move)
+              chrome.tabs.move(tab.id, { index: -1 });
+          });
+          
+          // Close if it's not first new tab
+          if(found)
+          {
+            chrome.storage.sync.get("close", function(data){
+              if(data.close)
+                chrome.tabs.remove(tab.id);  
+            });
+          }
+      
+          found = true;
+        }
+      }
+      
+      if(!found)
+        chrome.tabs.create({ url: "chrome://newtab/", active: false }, function(new_tab){});
+    });
+    
+    
+    // Group check
+    chrome.storage.sync.get("group", function(data){
+      if(!data.group)
+        return;
+      
+      chrome.tabGroups.query({}, function(groups){
+        if(!groups)
+          return;
+        
+        for(const group of groups)
+        {
+          chrome.tabs.query({windowType:'normal', groupId:group.id}, function(tabs){
+            var found = false;
+            for(const tab of tabs)
+            {
+              if(tab.url == "chrome://newtab/")
+              {
+                // Move new tabs forward
+                chrome.storage.sync.get("move", function(data){
+                  // Check for amount of tabs to avoid moving the group
+                  if(data.move && tabs.length > 2)
+                  {
+                    chrome.tabs.move(tab.id, { index: -1 });
+                    chrome.tabs.group({tabIds: tab.id, groupId: group.id});                    
+                  }
+                });
+                
+                // Close if it's not first new tab
+                if(found)
+                {
+                  chrome.storage.sync.get("close", function(data){
+                    if(data.close)
+                      chrome.tabs.remove(tab.id);  
+                  });
+                }
+            
+                found = true;
+              }
+            }
+            
+            if(!found)
+            {
+              chrome.tabs.create({ url: "chrome://newtab/", active: false }, function(new_tab){
+                chrome.tabs.group({tabIds: new_tab.id, groupId: group.id});
+              });
+            }
+          });
+        }
+      });
+    });
+  });
 }
