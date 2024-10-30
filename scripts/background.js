8ce38bf67fea1bef03eaa8a535f2ce823bc1fef7
Illia Vidanov <illia.vidanov@gmail.com>
2024-10-30 14:41:31 +0100

    Clean up and TODO

--- scripts/background.js
+++ scripts/background.js
@@ -1,79 +1,58 @@
+// Check if tab was removed
 chrome.tabs.onRemoved.addListener(OnRemoved);
-chrome.tabs.onUpdated.addListener(OnUpdated);
-
-chrome.runtime.onMessage.addListener(
-function(request, sender, sendResponse)
-{
-    if(request.msg == "Check") Check();
-});
-
 function OnRemoved(tabId, removeInfo)
 {
 	Check();
 }
 
+// Check if url changed
+chrome.tabs.onUpdated.addListener(OnUpdated);
 function OnUpdated(tabId, changeInfo, tab)
 {
 	if(changeInfo.url)
 		Check();
 }
 
+// Popup refresh message
+chrome.runtime.onMessage.addListener(
+function(request, sender, sendResponse)
+{
+    if(request.msg == "Check") Check();
+});
+
 function Check()
 {
-	chrome.storage.sync.get("enabled",
-	function(data)
+	chrome.storage.sync.get("enabled", function(data){
+	if(!data.enabled)
+		return;
+	
+	chrome.tabs.query({windowType:'normal'}, function(tabs){
+	var found = false;
+	for(const tab of tabs)
 	{
-		if(!data.enabled)
-			return;
-		
-		chrome.windows.getCurrent(
-		function(window)
+		if(tab.url == "chrome://newtab/" && !tab.pinned)
 		{
-			while(chrome.runtime.lastError)
-			{
-				setTimeout(Check, 100);
-			}
-
-			if(window.type != "normal")
-				return;
-
-			chrome.tabs.query({ currentWindow: true },
-			function(tabs)
-			{
-				// Querry tabs to check if there is new tab
-				var found = false;
-				for(const tab of tabs)
-				{
-					if(tab.url == "chrome://newtab/" && !tab.pinned)
-					{
-						// move new tabs forward
-						chrome.storage.sync.get("move",
-						function(data)
-						{
-							if(data.move)
-								chrome.tabs.move(tab.id, { index: -1 });
-						});
-						
-						// Close if it's not first new tab
-						if(found)
-						{
-							chrome.storage.sync.get("close",
-							function(data)
-							{
-								if(data.close)
-									chrome.tabs.remove(tab.id);	
-							});
-						}
-		
-						found = true;
-					}
-				}
-				
-				if(!found)
-				{
-					chrome.tabs.create({ url: "chrome://newtab/", active: false }, function (newTab) {});
-				}
+			// Move new tabs forward
+			chrome.storage.sync.get("move", function(data){
+				if(data.move)
+					chrome.tabs.move(tab.id, { index: -1 });
 			});
-		});
+			
+			// Close if it's not first new tab
+			if(found)
+			{
+				chrome.storage.sync.get("close", function(data){
+					if(data.close)
+						chrome.tabs.remove(tab.id);	
+				});
+			}
+	
+			found = true;
+		}
+	}
+	
+	if(!found)
+		chrome.tabs.create({ url: "chrome://newtab/", active: false }, function(newTab){});
+	});
 	});
 }
